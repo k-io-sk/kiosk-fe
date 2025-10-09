0fc7c1db5537c83e76f7e50a05f19767d6b2cbe3
shinchaerin79 <shinchaerin79@gmail.com>
2025-10-10 03:45:46 +0900

    :sparkles: Feat: 홈 화면 섹션 UI 구현

--- src/components/home/intro/IntroSection.jsx
+++ src/components/home/intro/IntroSection.jsx
@@ -6,7 +6,11 @@ export default function IntoSection() {
   return (
     <SectionWrapper>
       <div className={styles.intro} style={{ backgroundImage: `url(${introImg})` }}>
-        <p className={styles.title}>하루의 쉼, 인사동에서</p>
+        <div className={styles.titleBox}>
+          <div className={`${styles.line} ${styles.topLine}`}></div>
+          <p className={styles.title}>하루의 쉼, 인사동에서</p>
+          <div className={`${styles.line} ${styles.bottomLine}`}></div>
+        </div>
       </div>
     </SectionWrapper>
   );
--- src/components/home/performance/PerformanceSection.jsx
+++ src/components/home/performance/PerformanceSection.jsx
@@ -1,10 +1,42 @@
 import styles from './PerformanceSection.module.css';
 import SectionWrapper from '../commons/SectionWrapper';
+import performanceImg from '@assets/images/performance.png';
+import img from '@assets/images/img.png';
 
 export default function PerformanceSection() {
   return (
     <SectionWrapper>
-      <p className={styles.title}>공연</p>
+      <div className={styles.backgroundImage} style={{ backgroundImage: `url(${performanceImg})` }}>
+        <div className={styles.overlay} />
+        <div className={styles.content}>
+          <div className={styles.gallery}>
+            <div className={styles.card}>
+              <img src={img} alt='festival 1' />
+            </div>
+            <div className={styles.card}>
+              <img src={img} alt='festival 2' />
+            </div>
+            <div className={styles.card}>
+              <img src={img} alt='festival 3' />
+            </div>
+          </div>
+
+          <div className={styles.description}>
+            <div className={styles.vline} />
+            <div className={styles.texts}>
+              <h2 className={styles.title}>공연</h2>
+              <p className={styles.desc}>
+                예술이 살아 숨쉬는 무대,
+                <br />
+                인사동 공연에서 특별한 순간을 만나보세요
+              </p>
+              <button className={styles.linkBtn} type='button'>
+                보러가기 →
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </SectionWrapper>
   );
 }
