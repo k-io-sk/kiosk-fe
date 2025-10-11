9f0710ecef48273f7116347ea15fd8591d685054
uni-j-uni <unijun0109@gmail.com>
2025-10-11 14:32:09 +0900

    :sparkles:Feat: 이벤트 목록 페이지 추가

--- src/pages/EventListPage.jsx
+++ src/pages/EventListPage.jsx
@@ -1,5 +1,39 @@
-import { Outlet } from 'react-router-dom';
+import { EventCard } from '@components/eventListPage';
+import { useState, useEffect } from 'react';
+import styles from './EventListPage.module.css';
 
 export default function EventListPage() {
-  return <Outlet />;
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const dummyData = [
+      {
+        id: 1,
+        thumbnail: '/images/event1.png',
+        title: 'AW2025 인사아트위크',
+        startDate: '2025-06-04',
+        endDate: '2025-06-14',
+        place: '인사동 아트센터',
+      },
+      {
+        id: 2,
+        thumbnail: '/images/event2.png',
+        title: '현대미술 특별전',
+        startDate: '2025-09-01',
+        endDate: '2025-09-31',
+        place: '국립현대미술관',
+      },
+    ];
+    setEvents(dummyData);
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.grid}>
+        {events.map((event) => (
+          <EventCard key={event.id} event={event} />
+        ))}
+      </div>
+    </div>
+  );
 }
