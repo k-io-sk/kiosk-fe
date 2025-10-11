787f622f12118b377842a8b19f9d92121186292c
uni-j-uni <unijun0109@gmail.com>
2025-10-11 14:32:37 +0900

    :recycle:Refactor: Router 구조 lazy 변경

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,19 +1,46 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 import { MainLayout } from '../layouts';
-import EventListPage from '../pages/EventListPage';
-import EventDetailPage from '../pages/EventDetailPage';
-import HomePage from '../pages/HomePage';
 
-export default function Routers() {
+const HomePage = lazy(() => import('../pages/HomePage'));
+const EventListPage = lazy(() => import('../pages/EventListPage'));
+const EventDetailPage = lazy(() => import('../pages/EventDetailPage'));
+const NotFoundPage = lazy(() => import('../pages/NotFound.jsx'));
+
+const PageLoader = () => (
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontSize: '16px',
+      color: '#666',
+    }}
+  >
+    페이지를 불러오는 중...
+  </div>
+);
+
+export default function AppRouter() {
+  const routes = [
+    { path: '', element: <HomePage /> },
+    { path: 'events', element: <EventListPage /> },
+    { path: 'events/:id', element: <EventDetailPage /> },
+    { path: '*', element: <NotFoundPage /> },
+  ];
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<HomePage />} />{' '}
-          <Route path="/events" element={<EventListPage />} />
-          <Route path="/events/:id" element={<EventDetailPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <Suspense fallback={<PageLoader />}>
+        <Routes>
+          <Route path='/' element={<MainLayout />}>
+            {routes.map(({ path, element }) => (
+              <Route key={path} path={path} element={element} />
+            ))}
+          </Route>
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
