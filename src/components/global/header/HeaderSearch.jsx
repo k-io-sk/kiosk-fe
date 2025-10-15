d4b9db20270744c384fb6aa9effe4294fb625c35
shinchaerin79 <shinchaerin79@gmail.com>
2025-10-15 10:52:45 +0900

    :art: Design: HeaderSearch 컴포넌트에 IoSearch 아이콘 적용

--- src/components/global/header/HeaderSearch.jsx
+++ src/components/global/header/HeaderSearch.jsx
@@ -1,6 +1,6 @@
 import { useLocation } from 'react-router-dom';
 import styles from './HeaderSearch.module.css';
-import searchIcon from '@assets/images/search.png';
+import { IoSearch } from 'react-icons/io5';
 
 export default function HeaderSearch() {
   const location = useLocation();
@@ -9,7 +9,7 @@ export default function HeaderSearch() {
     <form className={`${styles.form} ${isIntroPage ? styles.transparentForm : ''}`}>
       <input className={`${styles.input} ${isIntroPage ? styles.whiteInput : ''}`} type='search' placeholder='검색' />
       <button className={styles.button} type='button'>
-        <img src={searchIcon} alt='검색' className={styles.searchIconicon} />
+        <IoSearch className={styles.searchIcon} />
       </button>
     </form>
   );
