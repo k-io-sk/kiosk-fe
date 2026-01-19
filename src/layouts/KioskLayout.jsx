d4353e334e5011d595d5403695932b79b94002a0
shinchaerin79 <shinchaerin79@gmail.com>
2026-01-19 19:28:53 +0900

    :art: Design: 키오스크 레이아웃 및 헤더/푸터 고정 구조 개선

--- src/layouts/KioskLayout.jsx
+++ src/layouts/KioskLayout.jsx
@@ -1,10 +1,22 @@
 import { Outlet } from 'react-router-dom';
 import styles from './KioskLayout.module.css';
+import KioskHeader from '@/components/global/header/KioskHeader';
+import KioskFooter from '@/components/global/footer/KioskFooter';
 
 export default function KioskLayout() {
   return (
-    <div className={styles.kioskLayout}>
-      <Outlet />
+    <div className={styles.shell}>
+      <header className={styles.header}>
+        <KioskHeader />
+      </header>
+
+      <main className={styles.main}>
+        <Outlet />
+      </main>
+
+      <footer className={styles.footer}>
+        <KioskFooter />
+      </footer>
     </div>
   );
 }
