29192af43d236b406655b1a809850c21f3554aeb
shinchaerin79 <shinchaerin79@gmail.com>
2026-01-20 01:59:05 +0900

    :art: Refactor: 키오스크 UI Context 추가 및 레이아웃 전역 상태 분리

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { KioskUIProvider } from './contexts/KioskUIContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <KioskUIProvider>
+      <App />
+    </KioskUIProvider>
   </StrictMode>,
 );
