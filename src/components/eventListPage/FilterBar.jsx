f11febbfdf12dddb849ba41ac6f695d009701e27
shinchaerin79 <shinchaerin79@gmail.com>
2026-01-19 17:16:16 +0900

    :recycle: Refactor: 모바일 행사 리스트 필터 API 연동

--- src/components/eventListPage/FilterBar.jsx
+++ src/components/eventListPage/FilterBar.jsx
@@ -1,19 +1,17 @@
 import styles from './FilterBar.module.css';
 import { useEffect, useState } from 'react';
-import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 export default function FilterBar({
   onFilterChange,
   selectedCategoryLabel = '전체',
   className = '',
-  showArrows = false,
-  onPrev,
-  onNext,
+  categories = ['전체', '공연', '전시', '기타'],
 }) {
-  const categories = ['전체', '공연', '전시', '기타'];
   const [selectedCategory, setSelectedCategory] = useState(selectedCategoryLabel);
 
-  useEffect(() => setSelectedCategory(selectedCategoryLabel), [selectedCategoryLabel]);
+  useEffect(() => {
+    setSelectedCategory(selectedCategoryLabel);
+  }, [selectedCategoryLabel]);
 
   const handleCategoryClick = (category) => {
     setSelectedCategory(category);
@@ -23,31 +21,17 @@ export default function FilterBar({
   return (
     <div className={`${styles.filterBar} ${className}`}>
       <ul className={styles.categoryList}>
-        {showArrows && (
-          <li className='kioskArrow'>
-            <button type='button' className={styles.arrowBtn} onClick={onPrev}>
-              <FiChevronLeft size={40} />
-            </button>
-          </li>
-        )}
-
         {categories.map((category) => (
           <li
             key={category}
             className={`${styles.categoryItem} ${selectedCategory === category ? styles.active : ''}`}
             onClick={() => handleCategoryClick(category)}
+            role='button'
+            tabIndex={0}
           >
             {category}
           </li>
         ))}
-
-        {showArrows && (
-          <li className='kioskArrow'>
-            <button type='button' className={styles.arrowBtn} onClick={onNext}>
-              <FiChevronRight size={40} />
-            </button>
-          </li>
-        )}
       </ul>
     </div>
   );
